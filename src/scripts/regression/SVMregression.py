67704b10399f01f97841549ebbe82eb983c1c725
lm-b <45048802+lm-b@users.noreply.github.com>
2019-06-05 12:41:51 -0700

    Update SVMregression.py

--- src/scripts/regression/SVMregression.py
+++ src/scripts/regression/SVMregression.py
@@ -19,7 +19,7 @@ import tensorflow as tf
 # main and/or supporting functions?
 
 
-# from hcp_config, get subjids and behaviroal data attributes
+# from hcp_config, get subjids and behavioral data attributes
 
 # split subjids into train and tes
 
@@ -33,10 +33,9 @@ import tensorflow as tf
 #update svm
 
 
-sess = tf.Session()
-testid=np.random.choice(len(subjID),round(len(subjID)/10)) # subjects in 10% test set
 
-def apply_encoder(subjid, tfrecordloc, encoder, testtrainID):
+
+def apply_encoder(subjid, tfrecordloc, encoder):
     """
     applies autoencoder to subject scans and assignes feature vectors to test and train sets
     """
@@ -48,37 +47,72 @@ def varbatch_svm(x_vals_train, x_vals_test, y_vals_train, y_vals_test, batch_siz
     for training data, the features for testing, the labels for training, and the labels for testing, the 
     batch size, the learning rate, and the threshold for acceptable error
     """
-#define placeholder vars
-feat= tf.placeholder(dtype=tf.float32,shape=[None, 1])
-label= tf.placeholder(dtype=tf.float32, shape=[None, 1])
-w = tf.Variable(tf.random_normal(shape=[1,1]))
-b = tf.Variable(tf.random_normal(shape=[1,1]))
-#define output
-svm_out = tf.add(tf.matmul(feat, w), b)
-#define constants 
-epsilon = tf.constant([eps])
-#define loss function: max(0,|w*x+b|-y-eps)
-loss = tf.reduce_mean(tf.maximum(0., tf.subtract(tf.abs(tf.subtract(model_out, label)), epsilon)))
-#define optimizer: gradient descent 
-my_opt = tf.train.GradientDescentOptimizer(LR)
-train_step = my_opt.minimize(loss)
-init = tf.initialize_all_variables()
-sess.run(init)
+    #define placeholder vars
+    feat= tf.placeholder(dtype=tf.float32,shape=[None, 1])
+    label= tf.placeholder(dtype=tf.float32, shape=[None, 1])
+    w = tf.Variable(tf.random_normal(shape=[1,1]))
+    b = tf.Variable(tf.random_normal(shape=[1,1]))
+    #define output
+    svm_out = tf.add(tf.matmul(feat, w), b)
+    #define constants 
+    epsilon = tf.constant([eps])
+    #define loss function: max(0,|w*x+b|-y-eps)
+    loss = tf.reduce_mean(tf.maximum(0., tf.subtract(tf.abs(tf.subtract(model_out, label)), epsilon)))
+    #define optimizer: gradient descent 
+    my_opt = tf.train.GradientDescentOptimizer(LR)
+    train_step = my_opt.minimize(loss)
+    init = tf.initialize_all_variables()
+    sess.run(init)
+    
+    train_loss = []
+    test_loss = []
+    
+    #run the function
+    for i in range(200):
+        rand_index = np.random.choice(len(x_vals_train), size=batch_size)
+        X = np.transpose([x_vals_train[rand_index]])
+        Y = np.transpose([y_vals_train[rand_index]])
+        sess.run(train_step, feed_dict={feat: X, label: Y})
+        temp_train_loss = sess.run(loss, feed_dict={x_data: np.transpose([x_vals_train]), y_target: np.transpose([y_vals_train])})
+        train_loss.append(temp_train_loss)
+        temp_test_loss = sess.run(loss, feed_dict={x_data:np.transpose([x_vals_test]), y_target: np.transpose([y_vals_test])})
+        test_loss.append(temp_test_loss)
 
-train_loss = []
-test_loss = []
 
-#run the function
-for i in range(200):
-    rand_index = np.random.choice(len(x_vals_train), size=batch_size)
-    X = np.transpose([x_vals_train[rand_index]])
-    Y = np.transpose([y_vals_train[rand_index]])
-    sess.run(train_step, feed_dict={feat: X, label: Y})
-    temp_train_loss = sess.run(loss, feed_dict={x_data: np.transpose([x_vals_train]), y_target: np.transpose([y_vals_train])})
-    train_loss.append(temp_train_loss)
-    temp_test_loss = sess.run(loss, feed_dict={x_data:np.transpose([x_vals_test]), y_target: np.transpose([y_vals_test])})
-    test_loss.append(temp_test_loss)
 
-for i in range(1,40):
-    testtrainid=np.random.choice(10,1)
-    print(testtrainid, testtrainid>0)
+
+# Start main section of code
+        
+sess = tf.Session()
+# get subject ID's and behavioral features from hcp_config
+subjid=hcp_config.py
+testid=np.random.choice(len(subjID),round(len(subjID)/15)) # choose subjects for 15% test set
+batch_size=20; #2% batch size for svm mbgd
+
+for id in subjID:
+    # Read TFRecord file
+    reader = tf.TFRecordReader()
+    filename_queue = tf.train.string_input_producer([str(id)+'.tfrecord'])
+    
+    _, serialized_example = reader.read(filename_queue)
+    # Define features
+    read_features = {
+            'scan': tf.FixedLenFeature([], dtype=tf.int64),
+            'behavioral': tf.FizedLenFeature(dtype=tf.string),
+            'label': tf.FixedLenFeature(dtype=tf.float32)
+            }
+    
+    # Extract features from serialized data
+    read_data = tf.parse_single_example(serialized=serialized_example,features=read_features)
+    
+    # Many tf.train functions use tf.train.QueueRunner,
+    # so we need to start it before we read
+        tf.train.start_queue_runners(sess)
+        
+    # Print features
+    for name, tensor in read_data.items():
+        print('{}: {}'.format(name, tensor.eval()))
+            
+
+
+
